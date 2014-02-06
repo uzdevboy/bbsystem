a73144f5c8be516757cc413f8c4e6f4ebf836066
Habib <habib8321@gmail.com>
2014-02-06 15:51:29 +0500

    change home page demo

--- www/index.php
+++ www/index.php
@@ -5,3 +5,19 @@
  * Time: 14:43
  */
 require_once "db/database.php";
+require_once "system/App.php";
+$site = new App();
+
+$site->setAppTitle("BBSystem");
+$site->setAppDesc("This is a Demo BBSystem");
+$site->getAppTags(array("demo" , "app" ,"bb" , "system"));
+?>
+<html>
+    <head>
+        <title><?=$site->getAppTitle();?></title>
+    </head>
+    <body>
+        <h1><?=$site->getAppTitle();?></h1>
+        <h2><?=$site->getAppDesc();?></h2>
+    </body>
+</html>
