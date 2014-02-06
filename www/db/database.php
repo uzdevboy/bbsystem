28bbf11d13b8698d8418fb95ff7a6c50527d29d6
Habib <habib8321@gmail.com>
2014-02-06 14:46:09 +0500

    Project Setup Files  , Database Connecttion

---
+++ www/db/database.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Habib
+ * Date: 06.02.14
+ * Time: 14:19
+ * To change this template use File | Settings | File Templates.
+ */
+require_once "protected/defines.php";
+class database
+{
+    private $host;
+    private $db_user;
+    private $db_pass;
+    private $db_name;
+
+    public function __construct(){
+        $this->host = HOST;
+        $this->db_user = DB_USER;
+        $this->db_pass = DB_USER_PASSWORD;
+        $this->db_name = DB_NAME;
+        $this->connect($this->host , $this->db_user , $this->db_pass , $this->db_name);
+    }
+
+    private  function connect($h , $u , $p , $n){
+        mysql_connect($h ,$u ,$p) or die("Cannot connect to database ! <br> See log : ".mysql_error());
+        mysql_select_db($n) or die("Cannot select to database ! <br/> See log : ".mysql_error());
+    }
+
+}
+$db = new database();
